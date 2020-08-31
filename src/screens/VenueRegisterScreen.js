28adc0a2bdd11ab6cad3839e39277b87945a0f92
Dhruv Seth <dhruvseth@Dhruvs-MBP.fios-router.home>
2020-08-30 23:31:24 -0400

    Initial commit

---
+++ src/screens/VenueRegisterScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const VenueRegisterScreen = () => {
+  return <View>
+    <Text>Venue Register Screen</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default VenueRegisterScreen;
