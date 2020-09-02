d9a755f947e8dc493b09d61bcff893507a479251
Dhruv Seth <dhruvseth@Dhruvs-MBP.fios-router.home>
2020-09-02 03:25:15 -0400

    Cleaned up code

--- src/screens/VenueTab.js
+++ src/screens/VenueTab.js
@@ -15,45 +15,58 @@ const VenueTab = ({ route }) => {
     style: {
       height: 80
     }}}>
-      <VenueTabNav.Screen name="VenuePromoters" options={{
-        tabBarIcon: ({ focused }) => (
-          <Ionicons name="ios-contact" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
-        ),
-        headerShown: false,
-        title: "Promoters"
-      }} component={VenuePromotersScreen} initialParams={{formData: route.params.form}} />
+      <VenueTabNav.Screen name="VenuePromoters"
+        options={{
+          tabBarIcon: ({ focused }) => (
+              <Ionicons name="ios-contact" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
+          ),
+          headerShown: false,
+          title: "Promoters"
+        }}
+        component={VenuePromotersScreen}
+        initialParams={{formData: route.params.form}} />
 
-      <VenueTabNav.Screen name="VenueEvents" options={{
-        tabBarIcon: ({ focused }) => (
-          <Ionicons name="ios-calendar" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
-        ),
-        headerShown: false,
-        title: "Events"
-      }} component={VenueEventsScreen} initialParams={{formData: route.params.form}} />
+      <VenueTabNav.Screen name="VenueEvents"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="ios-calendar" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
+          ),
+          headerShown: false,
+          title: "Events"
+        }}
+        component={VenueEventsScreen}
+        initialParams={{formData: route.params.form}} />
 
-      <VenueTabNav.Screen name="VenueMessages" options={{
-        tabBarIcon: ({ focused }) => (
-          <Ionicons name="ios-chatboxes" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
-        ),
-        headerShown: false,
-        title: "Inbox"
-      }} component={() => null} />
+      <VenueTabNav.Screen name="VenueMessages"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="ios-chatboxes" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
+          ),
+          headerShown: false,
+          title: "Inbox"
+        }}
+        component={() => null} />
 
-      <VenueTabNav.Screen name="VenueLedger" options={{
-        tabBarIcon: ({ focused }) => (
-          <Ionicons name="ios-pricetags" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
-        ),
-        headerShown: false,
-        title: "Ledger"
-      }} component={() => null} />
+      <VenueTabNav.Screen name="VenueLedger"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="ios-pricetags" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
+          ),
+          headerShown: false,
+          title: "Ledger"
+        }}
+        component={() => null} />
 
-      <VenueTabNav.Screen name="VenueSettings" options={{
-        tabBarIcon: ({ focused }) => (
-          <Ionicons name="md-settings" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
-        ),
-        headerShown: false,
-        title: "Settings"
-      }} component={() => null} />
+      <VenueTabNav.Screen
+        name="VenueSettings"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="md-settings" size={32} color={focused ? "#562093" : 'gray'} style={{"marginTop": 6}}/>
+          ),
+          headerShown: false,
+          title: "Settings"
+        }}
+        component={() => null} />
     </VenueTabNav.Navigator>
   )
 }
