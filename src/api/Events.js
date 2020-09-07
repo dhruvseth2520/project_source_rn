feeea4c96adae20582f3ea49844daf6343c5bda4
Maxence Scheercousse <maxence.scheercousse@flymya.co>
2020-09-07 16:25:38 +0630

    Testing out api folder

---
+++ src/api/Events.js
@@ -0,0 +1,28 @@
+export const getEvents = () => {
+    return [
+        {
+            event_id: 1,
+            title: '20% Off!',
+            venueName: "Harry's",
+            image: "https://us.123rf.com/450wm/anton345/anton3451611/anton345161100032/68970620-happy-hour-new-vintage-headline-sign-design-with-a-banner-ribbon-for-text-vector-graphic-.jpg?ver=6"
+        },
+        {
+            event_id: 2,
+            title: 'Ladies Night',
+            venueName: "Escape",
+            image: "https://penji.co/wp-content/uploads/2019/04/the-break-free-bar-promotion.jpg"
+        },
+        {
+            event_id: 3,
+            title: 'Buy 2 get 1 free',
+            venueName: "After8",
+            image: "https://scontent.frgn3-1.fna.fbcdn.net/v/t1.0-9/1511351_799473976779158_2312664901347705447_n.jpg?_nc_cat=108&_nc_sid=2d5d41&_nc_ohc=BFb397z07VYAX-5hPY-&_nc_ht=scontent.frgn3-1.fna&oh=b597db719d2baddfbfb770c410bbf9b8&oe=5F758C32"
+        },
+        {
+            event_id: 4,
+            title: 'Free beer if team A wins',
+            venueName: '50th Street',
+            image: "https://upserve.com/media/sites/2/Superbowl-Refresh-Blog-Post-1100x600.jpg"
+        }
+    ]
+}
