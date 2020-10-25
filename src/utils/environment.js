62c9d0e70c2b231f1f1b33ee3513806b4734a0de
Scheercuzy <maxi730@gmail.com>
2020-10-25 10:51:49 +0630

    Added online server api url

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -2,4 +2,8 @@ const env = {
   API_URL: 'http://192.168.1.171:3000'
 }
 
+const online = {
+  API_URL: 'http://default:jeKEoqmZqFp4fRGUPjT9@project_source.scheercousse.com'
+}
+
 export default env;
