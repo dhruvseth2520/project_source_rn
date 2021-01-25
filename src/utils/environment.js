1fe8712c6ef864dfbc4b9ba3f02ccd717ae81f20
Dhruv Seth <dhruvseth@MacBook-Pro.fios-router.home>
2021-01-24 21:36:47 -0500

    Testing bug

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,5 +1,5 @@
 const env = {
-  API_URL: 'http://192.168.1.56:3000'
+  API_URL: 'http://192.168.1.252:3000'
   // API_URL: 'http://10.0.0.173:3000',
   // API_URL: 'http://localhost:3000',
   // API_URL: 'https://default:jeKEoqmZqFp4fRGUPjT9@api.projectsource.app'
